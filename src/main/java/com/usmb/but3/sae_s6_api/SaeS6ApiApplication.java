1dae85e9c8d833038aa79c95ad7616bb71a884ed
Quentin LAVAL <lavalq@iut-acy.local>
2025-06-10 09:36:08 +0200

    Initial commit

---
+++ src/main/java/com/usmb/but3/sae_s6_api/SaeS6ApiApplication.java
@@ -0,0 +1,13 @@
+package com.usmb.but3.sae_s6_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeS6ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaeS6ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usmb/but3/sae_s6_api/SaeS6ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usmb.but3.sae_s6_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaeS6ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
