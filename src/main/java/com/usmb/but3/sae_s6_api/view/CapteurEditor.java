ec21db2db8d1d28658360b8efed4c81403c000ca
Quentin LAVAL <lavalq@iut-acy.local>
2025-06-20 11:49:23 +0200

    init capteur

---
+++ src/main/java/com/usmb/but3/sae_s6_api/view/CapteurEditor.java
@@ -0,0 +1,13 @@
+package com.usmb.but3.sae_s6_api.view;
+
+import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+
+@SpringComponent
+@UIScope
+public class CapteurEditor extends VerticalLayout implements KeyNotifier{
+
+
+}
---
+++ src/main/java/com/usmb/but3/sae_s6_api/view/CapteurView.java
@@ -0,0 +1,12 @@
+package com.usmb.but3.sae_s6_api.view;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+@Route("capteur")
+@PageTitle("Capteurs")
+@Menu(title = "Capteurs", order = 0, icon = "vaadin:clipboard-check")
+public class CapteurView extends VerticalLayout{
+    
+}
