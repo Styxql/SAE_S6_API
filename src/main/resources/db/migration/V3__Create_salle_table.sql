06d345bb30bb9cdaedcdb38c424914e4103e40a6
Maxime RUAULT <teamruault.maxime2@gmail.com>
2025-06-21 14:16:26 +0200

    Set Not NULL foreign key salle -> batiment

--- src/main/resources/db/migration/V3__Create_salle_table.sql
+++ src/main/resources/db/migration/V3__Create_salle_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE t_e_salle_sal (
     sal_nom VARCHAR(255) NOT NULL,
     sal_urlimg VARCHAR(1024), 
     sal_capacite INTEGER,
-    bat_id INTEGER,
+    bat_id INTEGER NOT NULL,
     typ_id INTEGER,
     FOREIGN KEY (bat_id) REFERENCES t_e_batiment_bat(bat_id),
     FOREIGN KEY (typ_id) REFERENCES t_e_typesalle_typ(typ_id)
